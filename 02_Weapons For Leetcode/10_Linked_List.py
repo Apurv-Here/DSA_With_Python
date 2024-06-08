8432948daa38e7d23b9f36233e460c033790e874
Apurv-Here <apurv.main2@gmail.com>
2024-06-08 17:27:42 +0530

    Added Linked List

--- 02_Weapons For Leetcode/10_Linked_List.py
+++ 02_Weapons For Leetcode/10_Linked_List.py
@@ -2,9 +2,57 @@
 """
 LINKED LIST
 
-START
+It is a dynamic data structure
+It is not continuous
+NODE: Elements of linked list
+
+Each node has date and the address of other element which it points
+Node -> Data & Link
+
+Last node points to NULL
+
+You can implement stack, queue and graphs
+
+-- Singly Linked List
+-- Doubly Linked List
+-- Circular Linked List
+
+There is no built in linked list in python so we will use class concepts
 
 """
+
+class Node:
+    def __init__(self, data):
+        self.data = data;
+        self.ref = None;
+        # We are note taking the reference in the arguments as value
+        # Bcz we want to set the reference as NONE (means empty value)
+
+# Now the nodes are created, we have to link them
+
+class LinkedList:
+    def __init__(self):
+        self.head = None;
+
+    def print_LL(self):
+        # self.head = n (for convenience, rather to write whole self.head.data/reference = n.data)
+        if self.head is None:
+            print("Linked List is empty!");
+        else:
+            n = self.head;
+
+            while n is not None:
+                print(n.data);
+                n = n.ref;
+
+if __name__ == "__main__":
+
+    # node1 = Node(10);
+
+    LL1 = LinkedList();
+    LL1.print_LL();
+
+
 
 
 
